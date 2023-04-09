ed819ab28c727639e5b7eb59e56ec0ad9bc2e1e5
SURYABRATA DAS <das.sukanta1598741@gmail.com>
2023-04-09 19:25:44 +0530

    updated

--- static/js/script.js
+++ static/js/script.js
@@ -1,2 +1,9 @@
 document.querySelector('input[type="file"]').addEventListener('change', function() {
-    if (this.files && this.files[0
+    if (this.files && this.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            document.querySelector('#imagePreview').setAttribute('src', e.target.result);
+        }
+        reader.readAsDataURL(this.files[0]);
+    }
+});
